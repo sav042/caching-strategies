0651b948d99bdd372229366b16c5929fc1a7b2b0
Винокуров Савелий <sav042@yandex.ru>
2024-01-02 15:43:53 +0300

    use map as db

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -3,28 +3,37 @@ package repository
 import (
 	"caching-strategies/internal/repository/entity/order"
 	"context"
-	"strconv"
+	"fmt"
+	"sync"
 )
 
-type Repo struct{}
+type Repo struct {
+	DB sync.Map
+}
 
 func New() *Repo {
 	return &Repo{}
 }
 
-func (r Repo) Get(ctx context.Context, IDs []uint64) (map[uint64]order.Order, error) {
-	// get data from db
+func (r *Repo) Get(ctx context.Context, IDs []uint64) (map[uint64]order.Order, error) {
 	ordersMap := make(map[uint64]order.Order, len(IDs))
-	// fill with mock data
-	for i := 0; i < len(IDs); i++ {
-		ordersMap[uint64(i)] = order.Order{
-			ID:   uint64(i),
-			Item: strconv.Itoa(i),
+
+	for _, ID := range IDs {
+		value, ok := r.DB.Load(ID)
+		if !ok {
+			return nil, fmt.Errorf("db loading error")
 		}
+		ord, ok := value.(order.Order)
+		if !ok {
+			return nil, fmt.Errorf("type casting error")
+		}
+		ordersMap[ord.ID] = ord
 	}
+
 	return ordersMap, nil
 }
 
-func (r Repo) Save(ctx context.Context, order *order.Order) (uint64, error) {
+func (r *Repo) Save(ctx context.Context, order *order.Order) (uint64, error) {
+	r.DB.Store(order.ID, *order)
 	return order.ID, nil
 }
